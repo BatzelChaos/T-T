d2a1d4a430461cb82fc97351865e23a2c4079749
BatzelChaos <101829864+BatzelChaos@users.noreply.github.com>
2024-10-16 18:54:59 +0300

    hello world :3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Player() { 
+        }
+        private static void Main(string[] args)
+        {
+            string username;
+            string playercolor;
+
+            Console.WriteLine("Choose PLAYER.COLOR");
+            Console.Write("Black\nBlue\nCyan\nDark Blue\nDark Cyan\nDark Gray\nDark Green\nDark Magenta");
+            Console.Write("\nDark Red\nDark Yellow\nGray\nGreen\nMagenta\nRed\nWhite\nYellow");
+            switch ()
+            {
+                case 
+            }
+
+            Console.Clear();
+            
+            
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Tyrants and Taverns, a world of awe and fear, friendships and hatred, and much more.");
+            Console.WriteLine("And... may I ask, who are you?");
+            Console.ForegroundColor = 
+            Console.Write("My name is ");
+            username = Console.ReadLine();
+            Console.WriteLine("Oh, yes, " + username + "! Is that a boy or a girl though?");
+            Console.WriteLine("");
+
+            Console.Write("\nPress any key to exit...")
+            Console.ReadKey();
+        }
+
+        private static void help()
+        {
+            if ("help" == Console.ReadLine())
+            {
+                Console.WriteLine("help system activated.");
+            }
+        }
+    }
+}
